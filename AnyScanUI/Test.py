04e54ced25a9cf68cf0ad4251740dbf98a7ecfb5
zhangzhenfeng <990768144@qq.com>
2017-02-24 17:34:06 +0800

    开始编写端口爆破任务的暂停，重启

--- AnyScanUI/Test.py
+++ AnyScanUI/Test.py
@@ -1,13 +1,24 @@
 from attack.AttackObject import AttackObject
 
 from attack.AttackBase import AttackBase
+import  threading,Queue,time
+
+threads_queue = Queue.Queue(maxsize = 10)
+
+
+class Attacker2(threading.Thread):
+    def run(self):
+        print 2
+        threads_queue.get()
+
+class Attacker1(threading.Thread):
+    def run(self):
+        print 1
+        threads_queue.get()
 
 if __name__ == "__main__":
-    # attackObject = AttackObject()
-    # attackObject.timeout = 20
-    # attackObject.threads = 30
-    # attacker = AttackBase(attackObject)
-    # attacker.attack()
-    l = ['192.168.1.24', '192.168.1.25', '192.168.1.22', '192.168.1.23', '192.168.1.20', '192.168.1.21', '192.168.1.7', '192.168.1.6', '192.168.1.5', '192.168.1.4', '192.168.1.3', '192.168.1.2', '192.168.1.1', '192.168.1.9', '192.168.1.8', '192.168.1.19', '192.168.1.18', '192.168.1.13', '192.168.1.12', '192.168.1.11', '192.168.1.10', '192.168.1.17', '192.168.1.16', '192.168.1.15', '192.168.1.14']
-    print l.sort(lambda x,y: cmp(''.join( [ i.rjust(3, '0') for i in x.split('.')] ), ''.join( [ i.rjust(3, '0') for i in y.split('.')] ) ) )
-    print l
+    for i in range(0,10):
+        threads_queue.put("")
+    print threads_queue.qsize()
+    threads_queue.queue.clear()
+    print threads_queue.qsize()
